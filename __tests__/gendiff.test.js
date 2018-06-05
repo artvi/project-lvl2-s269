8a21c35ee6b9e51221338fda2f9261e93482dbfb
artv <mona@lisa.io>
2018-06-06 02:54:06 +0300

    fix for Travis

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,13 @@
+import fs from 'fs';
 import diffFinder from '../src';
 
+
+const exPath = '__tests__/__fixtures__/expected.json';
+const expected = fs.readFileSync(exPath, 'utf8');
+
 test('comparing two files', () => {
-  const path1 = '../__tests__/before.json';
-  const path2 = '../__tests__/after.json';
-  const expected = 'src/__tests__/expected.json';
+  const path1 = '__tests__/__fixtures__/before.json';
+  const path2 = '__tests__/__fixtures__/after.json';
   const current = diffFinder(path1, path2);
   expect(current).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ export default (path1, path2) => {
   const allKeys = Array.from(new Set(befKeys.concat(aftKeys)));
   const result = allKeys.reduce((acc, el) => {
     if (bef[el] === aft[el]) {
-      return [...acc, `${el}: ${bef[el]}`];
+      return [...acc, `  ${el}: ${bef[el]}`];
     } else if (_.has(bef, el)) {
       return _.has(aft, el) ?
-        [...acc, `+ ${el}: ${aft[el]}`, `- ${el}: ${bef[el]}`] :
-        [...acc, `- ${el}: ${bef[el]}`];
-    } return [...acc, `+ ${el}: ${aft[el]}`];
+        [...acc, `  + ${el}: ${aft[el]}`, `  - ${el}: ${bef[el]}`] :
+        [...acc, `  - ${el}: ${bef[el]}`];
+    } return [...acc, `  + ${el}: ${aft[el]}`];
   }, []);
   const str = _.join(result, '\n');
-  return `{\n${str}\n}`;
+  return `{\n  ${str}\n}\n`;
 };
