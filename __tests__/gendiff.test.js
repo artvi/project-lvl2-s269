34d707c61988a0119ad9fb57a6c6e23593d81825
artv <mona@lisa.io>
2018-06-06 02:16:04 +0300

    fix for Travis

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,8 @@
 import diffFinder from '../src';
 
 test('comparing two files', () => {
-  const path1 = '__tests__/before.json';
-  const path2 = '__tests__/after.json';
+  const path1 = '../__tests__/before.json';
+  const path2 = '../__tests__/after.json';
   const expected = 'src/__tests__/expected.json';
   const current = diffFinder(path1, path2);
   expect(current).toBe(expected);
