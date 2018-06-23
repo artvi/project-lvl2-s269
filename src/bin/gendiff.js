35f447d761e19f2fdf5ed07937fe4701de1ac7c6
artv <mona@lisa.io>
2018-06-23 16:56:33 +0300

    cleaning dist

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import findDiffBetweenTwoFiles from '..';
 
 program
-  .version('0.2.1')
+  .version('0.2.2')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig>, <secondConfig>')
   .option('-f, --format [type]', 'output format: plain|json', 'tree')
